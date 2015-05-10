5d02a527b21892c781b21b751a7071b3d7e83229
Tyler Axdorff <Tyler.Axdorff1@T-Mobile.com>
2015-05-10 12:39:08 -0700

    added comments about plot code

--- plot1.R
+++ plot1.R
@@ -7,7 +7,11 @@ rawData$Date <- as.Date(rawData$Date, format="%d/%m/%Y")
 # Subset data to just Feb 1st 2007 - Feb 2nd 2007
 subsetData <- rawData[(rawData$Date >= "2007-02-01" & rawData$Date <= "2007-02-02"),]
 
-# Make the plot
+# Make the histogram of $Global_active_power
+# y-axis = Frequency
+# x-axis = Global Active Power (kilowatts)
+# title = Global Active Power
+# color = Red
 hist(subsetData$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
 
 # Save the plot to 'plot1.png'
